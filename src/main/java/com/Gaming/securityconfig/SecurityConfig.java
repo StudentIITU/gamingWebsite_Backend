de48fb39837f63b48fbb139acedc2e256caaf28c
nurbolatkoshkimbayev <31545@iitu.edu.kz>
2023-12-24 23:20:41 +0600

    Adding security configuration with some role based components;

--- src/main/java/com/Gaming/securityconfig/SecurityConfig.java
+++ src/main/java/com/Gaming/securityconfig/SecurityConfig.java
@@ -8,38 +8,34 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import java.beans.Customizer;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JWTAuthenticationFilter jwtAuthFiler;
+
+
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/public/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/gamer/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFiler, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
 }
---
+++ src/main/java/com/Gaming/services/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.Gaming.services;
+
+
+import com.Gaming.components.Gamer;
+import com.Gaming.components.Role;
+import com.Gaming.config.JWTService;
+import com.Gaming.controllers.AuthenticationRequest;
+import com.Gaming.controllers.RegisterRequest;
+import com.Gaming.repository.GamerRepo;
+import com.Gaming.responses.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final GamerRepo repo;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtservice;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var gamer = Gamer.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repo.save(gamer);
+        var jwtToken = jwtservice.generateToken(gamer);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var gamer = repo.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtservice.generateToken(gamer);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
